4399848b3bb973f8f99d740a9e5c0428585750e9
Terry Huang <terryh.tp@gmail.com>
2014-01-29 10:28:57 +0800

    fist commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-angular-pickadate-directive
-===========================
+# angular-pickadate-directive
+Bower Component for a simple AngularJS directive using [pickadate.js](http://amsul.ca/pickadate.js/). 
 
-AngularJS directive to use pickadate.js
+## Usage
+
+1. Made sure the pickadate lib and jquery are loaded.
+2. Add `ng.pickadate` as a module dependency to your angular app.
+3. Insert the `pick-Adate` or `pick-Atime` directive into your template:
+
+```html
+Date
+<input type="text" name="pickdate" pick-Adate ng-model="pickdate" />
+
+
+Time
+
+<input type="text" name="picktime" pick-Atime ng-model="picktime" />
+```
+## License
+MIT
---
+++ ng-pickadate.js
@@ -0,0 +1,41 @@
+/*
+ * angular-pickadate-directive v0.0.1
+ * (c) 2014 Terru Huang http://blog.lifetaiwan.net
+ * License: MIT
+ */
+
+'use strict';
+
+angular.module('ng.pickadate', []).
+  directive('pickAdate', [ '$parse',function($parse){
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var ngModel = $parse(attrs.ngModel);
+        element.pickadate({
+          format: 'yyyy/mm/dd',
+          onSet: function(e){
+            ngModel.assign(scope, this.get());
+          }
+
+        });
+      }
+    };
+
+}]).
+  directive('pickAtime', [ '$parse', function($parse){
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var ngModel = $parse(attrs.ngModel);
+        element.pickatime({
+          onSet: function(e){
+            ngModel.assign(scope, this.get());
+          }
+
+        });
+      }
+    };
+
+}]);
+
